552609840f9e77179ec00988d7395dde2e3f53b5
quonn <qber83@gmail.com>
2019-05-26 19:27:58 -0700

    deployment changes

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   TEST_DB_URL: process.env.DATABASE_URL || 'postgresql://quonn@localhost/blog_db',
-  DB_URL: process.env.DB_URL || 'postgresql://quonn@localhost/blog_db',
+  DB_URL: process.env.DATABASE_URL || 'postgresql://quonn@localhost/blog_db',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '6000ms',
 };
