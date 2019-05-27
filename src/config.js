852da8798fad52311596e17ae62ad431d60f3bbe
quonn <qber83@gmail.com>
2019-05-26 20:23:31 -0700

    updated API_ENDPOINT

--- src/config.js
+++ src/config.js
@@ -7,4 +7,6 @@ module.exports = {
   DB_URL: process.env.DATABASE_URL || 'postgresql://quonn@localhost/blog_db',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '6000ms',
+  API_ENDPOINT: `https://protected-tundra-30619.herokuapp.com/`,
+  API_KEY: process.env.REACT_APP_API_KEY,
 };
